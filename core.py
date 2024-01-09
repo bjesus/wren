037c20e05f093d8623dfe3287e588eb2216d9a0e
Yo'av Moshe <github@yoavmoshe.com>
2024-01-09 22:41:59 +0100

    initial commit

---
+++ core.py
@@ -0,0 +1,134 @@
+import os
+import shutil
+from pathvalidate import sanitize_filename
+import requests
+import json
+from datetime import datetime
+from platformdirs import user_data_dir, user_config_dir
+
+# import pprint
+# pp = pprint.PrettyPrinter().pprint
+
+notes_dir = os.path.expanduser("~/Notes")
+done_dir = os.path.join(notes_dir, "done")
+data_dir = user_data_dir("knowts", "knowts")
+config_dir = user_config_dir("knowts", "knowts")
+messages_log = os.path.join(data_dir, "messages.json")
+
+config = {
+    "mode": "single-user",
+    "openaiToken": "",
+    "telegramToken": "",
+    "notes_dir": os.path.expanduser("~/Notes"),
+}
+
+try:
+    with open(os.path.join(config_dir, "knowts.json"), "r") as file:
+        user_config = json.load(file)
+except FileNotFoundError:
+    user_config = {}
+config = {**config, **user_config}
+
+
+def mkdir(path):
+    if not os.path.exists(path):
+        os.makedirs(path)
+
+
+mkdir(data_dir)
+mkdir(notes_dir)
+mkdir(done_dir)
+
+
+def create_new_task(content):
+    filename = sanitize_filename(content.split("\n")[0])
+    content = "\n".join(content.split("\n")[1:])
+    with open(os.path.join(notes_dir, filename), "w") as file:
+        file.write(content)
+    return filename
+
+
+def get_tasks():
+    files = [
+        file
+        for file in os.listdir(notes_dir)
+        if os.path.isfile(os.path.join(notes_dir, file)) and not file.startswith(".")
+    ]
+    return files
+
+
+def get_summary():
+    url = "https://api.openai.com/v1/chat/completions"
+    current_time = datetime.now().isoformat()
+    tasks = get_tasks()
+    current_message = {"role": "user", "content": f"{current_time}\n{tasks}"}
+
+    try:
+        with open(messages_log, "r") as file:
+            existing_data = json.load(file)
+    except FileNotFoundError:
+        existing_data = []
+
+    payload = {
+        "model": "gpt-4",
+        "messages": [
+            {
+                "role": "system",
+                "content": "You are a helpful assistant that helps the user be on top of the schedule and tasks. every once in a while, the user is going to send you the current time and a list of currently pending tasks. your role is to tell the user in a simple language what they need to do today. IF AND ONLY IF a task has been ongoing for a long time, let the user know about it. IF AND ONLY IF you see a task that appeared earlier in the chat but doesn't appear anymore, add a small congratulation to acknowledge the fact that the task was completed. the user will send each task in a new line starting with a dash. words starting with a plus sign are tags related to task. when writing back to the user, try to mention tasks that share the same tags or concept together and be concise.",
+            },
+        ]
+        + existing_data
+        + [current_message],
+    }
+    headers = {
+        "content-type": "application/json",
+        "Authorization": "Bearer " + config["openaiToken"],
+    }
+
+    response = requests.post(url, json=payload, headers=headers)
+    data = response.json()
+    response = data["choices"][0]["message"]["content"]
+
+    existing_data.extend([current_message, data["choices"][0]["message"]])
+
+    with open(messages_log, "w") as file:
+        json.dump(existing_data, file, indent=2)
+
+    return response
+
+
+def get_task_file(name):
+    matching_files = [
+        file
+        for file in os.listdir(notes_dir)
+        if name.lower() in file.lower()
+        and os.path.isfile(os.path.join(notes_dir, file))
+    ]
+    if len(matching_files) == 1:
+        return (True, matching_files[0])
+    elif len(matching_files) > 1:
+        return (False, "Error: Multiple matching files found.")
+    else:
+        return (False, f"Error: No matching file for '{name}' found.")
+
+
+def mark_task_done(name):
+    found, filename = get_task_file(name)
+    if found:
+        shutil.move(os.path.join(notes_dir, filename), os.path.join(done_dir, filename))
+        response = f'marked "{filename} as done"'
+    else:
+        response = filename
+    return response
+
+
+def get_task_content(name):
+    found, filename = get_task_file(name)
+    if found:
+        file_to_read = os.path.join(notes_dir, filename)
+        with open(file_to_read, "r") as file:
+            file_content = file.read()
+            response = f"{filename}\n\n{file_content}"
+    else:
+        response = filename
+    return response
---
+++ telegram.py
@@ -0,0 +1,144 @@
+import telebot
+from apscheduler.schedulers.background import BackgroundScheduler
+from apscheduler.triggers.cron import CronTrigger
+import os
+import json
+from platformdirs import user_data_dir
+from core import (
+    create_new_task,
+    get_summary,
+    mark_task_done,
+    get_tasks,
+    get_task_content,
+    config,
+)
+
+bot = telebot.TeleBot(config["telegramToken"])
+scheduler = BackgroundScheduler()
+
+data_dir = user_data_dir("knowts", "knowts")
+schedules_path = os.path.join(data_dir, "schedules.json")
+
+
+def get_all_schedules():
+    try:
+        with open(schedules_path, "r") as file:
+            return json.load(file)
+    except FileNotFoundError:
+        return []
+
+
+@bot.message_handler(commands=["list"])
+def list_tasks(message):
+    tasks = get_tasks()
+    response = "".join(map(lambda t: "- " + t + "\n", tasks))
+    bot.send_message(message.chat.id, response)
+
+
+@bot.message_handler(commands=["summary"])
+def summary(message):
+    summary = get_summary()
+    bot.send_message(message.chat.id, summary)
+
+
+@bot.message_handler(commands=["done", "d", "do", "don"])
+def mark_as_done(message):
+    name = " ".join(message.text.split(" ")[1:])
+    response = mark_task_done(name)
+    bot.send_message(message.chat.id, response)
+
+
+@bot.message_handler(commands=["read"])
+def read_task(message):
+    name = " ".join(message.text.split(" ")[1:])
+    response = get_task_content(name)
+    bot.send_message(message.chat.id, response)
+
+
+@bot.message_handler(commands=["help"])
+def help(message):
+    bot.send_message(
+        message.chat.id,
+        "knowts\n\n- enter any text to save it as task\n- mark a task as done by `/done foo`",
+    )
+
+
+@bot.message_handler(commands=["start"])
+def start(message):
+    bot.set_my_commands(
+        [
+            telebot.types.BotCommand(command="help", description="Help"),
+            telebot.types.BotCommand(command="summary", description="Summary"),
+            telebot.types.BotCommand(command="list", description="List"),
+            telebot.types.BotCommand(command="schedule", description="Schedule"),
+        ]
+    )
+    bot.set_chat_menu_button(
+        message.chat.id, telebot.types.MenuButtonCommands("commands")
+    )
+    bot.send_message(message.chat.id, "knowts.\n\nuse /help to get help")
+
+
+@bot.message_handler(commands=["schedule"])
+def create_scheduled_message(message):
+    schedule = " ".join(message.text.split(" ")[1:])
+    job = [message.chat.id, schedule]
+
+    # read current schedules
+    schedules = get_all_schedules()
+
+    if not schedule:
+        my_schedules = [s[1] for s in schedules if s[0] == message.chat.id]
+        if my_schedules:
+            bot.send_message(
+                message.chat.id,
+                f'your summaries are scheduled for: {", ".join(my_schedules)}',
+            )
+        else:
+            bot.send_message(message.chat.id, f"you got nothing scheduled!")
+        return
+
+    # save new schedule
+    schedules.extend([job])
+    with open(schedules_path, "w") as file:
+        json.dump(schedules, file, indent=2)
+
+    # register it
+    scheduler.add_job(
+        send_summary,
+        CronTrigger.from_crontab(schedule),
+        kwargs={"chat_id": message.chat.id},
+    )
+    bot.send_message(message.chat.id, f"Added a schedule: {schedule}")
+
+
+@bot.message_handler(
+    func=lambda message: len(message.text) > 3 and not message.text.startswith("/")
+)
+def add(message):
+    filename = create_new_task(message.text)
+    bot.send_message(message.chat.id, "added: " + filename)
+
+
+@bot.message_handler(func=lambda message: True)
+def reply_no(message):
+    bot.reply_to(message, "not sure what you want. seek /help")
+
+
+def send_summary(chat_id):
+    bot.send_message(chat_id, get_summary())
+
+
+schedules = get_all_schedules()
+for schedule in schedules:
+    scheduler.add_job(
+        send_summary,
+        CronTrigger.from_crontab(schedule[1]),
+        kwargs={"chat_id": schedule[0]},
+    )
+
+
+def start_bot():
+    print("Starting telegram bot")
+    scheduler.start()
+    bot.polling()
