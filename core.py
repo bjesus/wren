28a569a6000ad1f5ee83ec3166106e0b8b50af8b
Yo'av Moshe <github@yoavmoshe.com>
2024-01-27 15:33:01 +0100

    remove future dir, and add simple types

--- core.py
+++ core.py
@@ -8,6 +8,7 @@ from dateutil import parser
 from platformdirs import user_data_dir, user_config_dir
 from croniter import croniter
 
+# Load config and set up folders
 
 data_dir = user_data_dir("knowts", "knowts")
 config_dir = user_config_dir("knowts", "knowts")
@@ -15,7 +16,6 @@ messages_log = os.path.join(data_dir, "messages.json")
 
 config = {
     "notes_dir": "~/Notes",
-    "future_dir": "~/Notes/future",
     "done_dir": "~/Notes/done",
     "openai_token": "",
     "telegram_token": "",
@@ -37,10 +37,11 @@ def parse_path(p, base=""):
 
 notes_dir = parse_path(config["notes_dir"])
 done_dir = parse_path(config["done_dir"], notes_dir)
-future_dir = parse_path(config["future_dir"], notes_dir)
+
+now = datetime.now()
 
 
-def mkdir(path):
+def mkdir(path: str) -> None:
     if not os.path.exists(path):
         os.makedirs(path)
 
@@ -50,79 +51,35 @@ mkdir(notes_dir)
 mkdir(done_dir)
 
 
-def create_new_task(content):
+# Common API
+
+
+def create_new_task(content: str) -> str:
     filename = sanitize_filename(content.split("\n")[0])
-    # check if future task
-    future = False
-    try:
-        parser.parse(filename.split(" ")[0])
-        future = True
-    except:
-        pass
-    try:
-        croniter(" ".join(filename.split(" ")[:5]))
-        future = True
-    except:
-        pass
     content = "\n".join(content.split("\n")[1:])
-    dest = future_dir if future else notes_dir
-    with open(os.path.join(dest, filename), "w") as file:
+    with open(os.path.join(notes_dir, filename), "w") as file:
         file.write(content)
     return filename
 
 
-def process_future():
+def get_tasks(query="") -> list[str]:
+    global now
     now = datetime.now()
-    for file in os.listdir(future_dir):
-        path = os.path.join(future_dir, file)
-        if os.path.isfile(path):
-            time = file.split(" ")[0]
-            if len(time) < 5:
-                # parse as cron
-                task_name = " ".join(file.split(" ")[5:])
-                cron = " ".join(file.split(" ")[:5])
-                last_task = None
-                for path in [
-                    os.path.join(notes_dir, task_name),
-                    os.path.join(done_dir, task_name),
-                ]:
-                    if os.path.exists(path):
-                        last_modified_date = datetime.fromtimestamp(
-                            os.path.getmtime(path)
-                        )
-                        if last_task is None or last_modified_date > last_task:
-                            last_task = last_modified_date
-                if not last_task or croniter(cron, last_task).get_next(datetime) <= now:
-                    shutil.copy(
-                        os.path.join(future_dir, file),
-                        os.path.join(notes_dir, task_name),
-                    )
-            else:
-                # parse as timestamp
-                try:
-                    task_time = parser.parse(time)
-                    if task_time <= now:
-                        shutil.move(path, notes_dir)
-                except:
-                    print("cannot parse future task: " + file)
-
-
-def get_tasks(s=""):
-    process_future()
-    files = [
-        file
-        for file in os.listdir(notes_dir)
+    return [
+        format_task_name(file)
+        for file in sorted(
+            os.listdir(notes_dir),
+            key=lambda x: os.path.getctime(os.path.join(notes_dir, x)),
+            reverse=True,
+        )
         if os.path.isfile(os.path.join(notes_dir, file))
         and not file.startswith(".")
-        and s in file
+        and query in file
+        and is_present_task(file)
     ]
-    return sorted(
-        files, key=lambda x: os.path.getctime(os.path.join(notes_dir, x)), reverse=True
-    )
 
 
-def get_summary():
-    process_future()
+def get_summary() -> str:
     url = "https://api.openai.com/v1/chat/completions"
     current_time = datetime.now().isoformat()
     tasks = get_tasks()
@@ -163,7 +120,7 @@ def get_summary():
     return response
 
 
-def get_task_file(name):
+def get_task_file(name: str) -> tuple[bool, str]:
     matching_files = [
         file
         for file in os.listdir(notes_dir)
@@ -178,17 +135,24 @@ def get_task_file(name):
         return (False, f"Error: No matching file for '{name}' found.")
 
 
-def mark_task_done(name):
+def mark_task_done(name: str) -> str:
     found, filename = get_task_file(name)
     if found:
-        shutil.move(os.path.join(notes_dir, filename), os.path.join(done_dir, filename))
+        if is_cron_task(filename):
+            shutil.copy(
+                os.path.join(notes_dir, filename), os.path.join(done_dir, filename)
+            )
+        else:
+            shutil.move(
+                os.path.join(notes_dir, filename), os.path.join(done_dir, filename)
+            )
         response = f'marked "{filename}" as done'
     else:
         response = filename
     return response
 
 
-def get_task_content(name):
+def get_task_content(name: str) -> str:
     found, filename = get_task_file(name)
     if found:
         file_to_read = os.path.join(notes_dir, filename)
@@ -198,3 +162,50 @@ def get_task_content(name):
     else:
         response = filename
     return response
+
+
+# Helper functions
+
+
+def is_present_task(file: str) -> bool:
+    if not file[0].isdigit():
+        return True
+    if is_cron_task(file):
+        cron = " ".join(file.split(" ")[:5])
+        last_task = None
+        path = os.path.join(done_dir, file)
+        if os.path.exists(path):
+            last_modified_date = datetime.fromtimestamp(os.path.getmtime(path))
+            if last_task is None or last_modified_date > last_task:
+                last_task = last_modified_date
+            if not last_task or croniter(cron, last_task).get_next(datetime) <= now:
+                return True
+    elif is_dated_task(file):
+        time = file.split(" ")[0]
+        task_time = parser.parse(time)
+        if task_time <= now:
+            return True
+    return False
+
+
+def format_task_name(filename: str) -> str:
+    if is_cron_task(filename):
+        return " ".join(filename.split()[5:])
+    if is_dated_task(filename):
+        return " ".join(filename.split()[1:])
+    return filename
+
+
+def is_dated_task(filename: str) -> bool:
+    try:
+        parser.parse(filename.split()[0])
+        return True
+    except:
+        return False
+
+
+def is_cron_task(filename: str) -> bool:
+    splitted = filename.split()
+    if len(splitted) < 6 or not all((s.isdigit() or s == "*") for s in splitted[:3]):
+        return False
+    return True
